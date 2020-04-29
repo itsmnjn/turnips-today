9c64731d1fee41069663b82c3ba1152af55a9b93
Min Jun Kim <mk7pe@virginia.edu>
2020-04-28 21:02:34 -0400

    Slightly shorten notification message

--- server/api.js
+++ server/api.js
@@ -436,9 +436,9 @@ const sendPush = (submission, tableName, dbConnection) => {
 	}
 
 	if (tableName === 'nook') {
-		dataToSend.title = `A new price of ${submission.price} bells from the Nook twins!`
+		dataToSend.title = `New price of ${submission.price} bells from the Nook twins!`
 	} else {
-		dataToSend.title = `A new price of ${submission.price} bells from Daisy!`
+		dataToSend.title = `New price of ${submission.price} bells from Daisy!`
 	}
 
 	dataToSend.message = 'Click me to go to the post.'
