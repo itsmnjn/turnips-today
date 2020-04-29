7ed58fe7eb37414d477c0591359d700726f7aeea
Min Jun Kim <mk7pe@virginia.edu>
2020-04-29 14:15:16 -0400

    Change hostname to turnips.today with HTTPS

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,2 @@
-export const apiURI = 'http://ec2-18-212-53-33.compute-1.amazonaws.com:8000/'
+export const apiURI = 'https://turnips.today:8000/'
 export const wsURI = 'ws://ec2-18-212-53-33.compute-1.amazonaws.com:8081/'
